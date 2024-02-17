d77fbeb1f3f9baee3f80afddd285cdddc93f2055
Seth Tor <sethtor@Seths-MacBook-Pro.local>
2024-02-17 11:20:44 -0800

    Added mock class test

--- src/main/java/com/promineotech/TestDemo.java
+++ src/main/java/com/promineotech/TestDemo.java
@@ -1,11 +1,40 @@
 package com.promineotech;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
 public class TestDemo {
 	public int addPositive(int a, int b) {
 		if (a <= 0 || b <= 0) {
 			throw new IllegalArgumentException("Both parameter must be positive!");
 		}
 		return a + b;
+	}
+	
+	public BigDecimal averageOfArray(double[] arr) {
+		if (arr.length == 0) {
+			throw new IllegalArgumentException("Array must have values.");
+		}
+		
+		double sum = 0;
+		for (double num : arr) {
+			sum += num;
+		}
+		double avg = sum / arr.length;
+		
+		return new BigDecimal(avg).setScale(2, RoundingMode.HALF_DOWN);
+	}
+	
+	public int randomNumberSquared() {
+		int randInt = getRandomInt();
+		
+		return randInt * randInt;
+	}
+	
+	int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) +1;
 	}
 
 }
--- src/test/java/com/promineotech/TestDemoJUnitTest.java
+++ src/test/java/com/promineotech/TestDemoJUnitTest.java
@@ -3,7 +3,11 @@ package com.promineotech;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -47,6 +51,42 @@ class TestDemoJUnitTest {
 		assertThat(testDemo.addPositive(10, 40)).isEqualTo(50);
 		assertThatThrownBy(() -> testDemo.addPositive(-5, 7)).isInstanceOf(IllegalArgumentException.class);
 		assertThatThrownBy(() -> testDemo.addPositive(0, 7)).isInstanceOf(IllegalArgumentException.class);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("com.promineotech.TestDemoJUnitTest#argumentsForAverageOfArray")
+	void assertThatAverageOfArrayIsCalculatedCorrectly(double[] arr, BigDecimal expected, boolean expectException) {
+		if (!expectException) {
+			assertThat(testDemo.averageOfArray(arr)).isEqualTo(expected);
+		} else {
+			assertThatThrownBy(() -> testDemo.averageOfArray(arr)).isInstanceOf(IllegalArgumentException.class);
+		}
+	}
+	
+	static Stream<Arguments> argumentsForAverageOfArray() {
+		double[] validArr = {2.5, 10.0, 50.25};
+		double[] validArrWithNegative = {2.5, -10.0, 50.25};
+		double[] emptyArr = {};
+		
+		return Stream.of(
+				arguments(validArr, new BigDecimal(20.92).setScale(2, RoundingMode.HALF_DOWN), false),
+				arguments(validArrWithNegative, new BigDecimal(14.25).setScale(2, RoundingMode.HALF_DOWN), false),
+				arguments(emptyArr, new BigDecimal(0).setScale(2, RoundingMode.HALF_DOWN), true)
+				);
+	}
+	
+	@Test
+	void assertThatNumberSquaredIsCorrect() {
+		TestDemo mockDemo = spy(testDemo);
+
+//		Given: number = 5
+		doReturn(5).when(mockDemo).getRandomInt();
+		
+//		When: square the number
+		int fiveSquared = mockDemo.randomNumberSquared();
+		
+//		Then: return 25
+		assertThat(fiveSquared).isEqualTo(25);
 	}
 
 }
